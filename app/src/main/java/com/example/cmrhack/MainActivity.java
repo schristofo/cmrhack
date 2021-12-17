a54ca7df9c68c2c0902808d482e5070db755e4dd
schristofo <schristofoece.auth.gr>
2021-12-17 22:58:18 +0200

    First commit

---
+++ app/src/main/java/com/example/cmrhack/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.cmrhack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TO-DO: Camera Fix Script
+            }
+        });
+    }
+}
